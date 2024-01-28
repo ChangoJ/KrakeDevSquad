8da3bdb5bc699d41393ca0f255bdde4d7e6349fe
JordanChango <jordan-chango@hotmail.com>
2024-01-27 20:38:42 -0500

    Mensaje saludo funcionando

--- test/test.js
+++ test/test.js
@@ -1,16 +1,21 @@
-saludar=function(){
+saludar = function () {
     let cmpNombre;
     let cmpApellido;
+    let cmpResultado;
     let nombre;
     let apellido;
     let saludo;
-   cmpNombre=document.getElementById("txtNombre");
-    cmpApellido=document.getElementById("txtApellido");
+    cmpNombre = document.getElementById("txtNombre");
+    cmpApellido = document.getElementById("txtApellido");
 
     nombre = cmpNombre.value;
     apellido = cmpApellido.value;
 
-    saludo=generarSaludo(nombre,apellido);
+    saludo = generarSaludo(nombre, apellido);
+
+    cmpResultado = document.getElementById("lblResultado")
+    cmpResultado.innerHTML = saludo
+
 
     console.log(saludo);
 }
